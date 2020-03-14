07db63ba63af4a8be2ec63830948ffce2b477b0e
klisban <moralesklisban@gmail.com>
2020-03-13 18:23:12 -0600

    Esta es la clase de vehiculo

---
+++ Vehiculo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Autor: Klisban Morales 0801199919211
+# Repositorio: https://github.com/moralesklisban9/Examen.git
+from datetime import datetime, timedelta
+plus_five_hours = datetime.now() + timedelta(hours=5)
+
+
+class Vehiculo:
+
+    hora = 0:
+    """
+    representa un vehiculo
+    """
+
+    def __init__(self, placa, marca, modelo, tipo):
+        """ Inicializar los objetos del vehiculo """
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.tipo = tipo
+        self.Hora_Entrada = hour()
+        self.Hora_Salida = hour()
+
+    
