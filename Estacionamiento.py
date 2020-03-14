95dd81a4d043a5b4961d0f08be59b32100099da2
klisban <moralesklisban@gmail.com>
2020-03-13 18:21:06 -0600

    Estacionamiento Examen II

---
+++ Estacionamiento.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Autor: Klisban Morales 0801199919211
+# Repositorio: https://github.com/moralesklisban9/Examen.git
+
+class Vehiculo():
+    
+    def __init__(self):
+        """ Docstrim: con esta def realizamos los self para la lista """
+        self.vehiculos = list()
+
+    def nuevo_vehiculo():
+        """ Docstrim: Creamos un vehiculo y lo añadimos """
+        self.vehiculos.append() "Llenar la lista"
+        return None
